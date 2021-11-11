8f2a03f308092f081126f9c776da538e914d91c9
lyorafelicya <16520164@std.stei.itb.ac.id>
2021-11-12 02:21:03 +0700

    add perbandingan

--- src/image_compression.py
+++ src/image_compression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from svd import svd
 from PIL import Image
 
@@ -15,24 +16,20 @@ def openImage(imagePath):
 
 #kompress setiap r,g,b
 def compression(A,k):
-    #U_color, S_color, V_color = np.linalg.svd(A)
     U_color, S_color, V_color = svd(A)
     imgCompressed = np.zeros((A.shape[0],A.shape[1]))
-    #tes print pake yg linalg sama fungsi svd kita
-    # print(U_color[:, 0:k])
-    # print(np.diag(S_color)[0:k,0:k])
-    US = np.matmul(U_color[:, 0:k],(S_color)[0:k,0:k])
-    imgReconstructed = np.matmul(US,V_color[0:k,:])
+    imgReconstructed = U_color[:, 0:k] @ (S_color)[0:k,0:k] @ V_color[0:k,:]
     imgCompressed = np.clip(imgReconstructed,0,255).astype('uint8')
     return imgCompressed
 
-
 np.set_printoptions(suppress=True)
 
-r,g,b, originalImage = openImage('16.png')
+r,g,b, originalImage = openImage('src/72.png')
 
-k = 5
+#input user
+k = 50
 
+#start_time = time.time()
 r_compressed = compression(r,k)
 g_compressed = compression(g,k)
 b_compressed = compression(b,k)
@@ -41,6 +38,31 @@ img_r = Image.fromarray(r_compressed, mode=None)
 img_g = Image.fromarray(g_compressed, mode=None)
 img_b = Image.fromarray(b_compressed, mode=None)
 
+#combine rgb
 compressedImage = Image.merge("RGB",(img_r,img_g,img_b))
 originalImage.show()
 compressedImage.show()
+final_time = time.time()
+
+#print(final_time - start_time)
+
+#Melihat perbandingan sebelum dan sesudah dikompres
+# image = np.array(Image.open('src/72.png'))
+
+# row = image.shape[0]
+# col = image.shape[1]
+# print('pixels: ', row ,'x',col)
+# originalSize = row * col * 3
+# compressedSize = k * (1 + row + col) * 3
+
+# print('original size:')
+# print(originalSize)
+
+# print('compressed size:')
+# print(compressedSize)
+
+# print('Ratio compressed size / original size:')
+# ratio = compressedSize/originalSize
+# print(ratio)
+
+# print('Compressed image size is ' + str(round(ratio * 100, 2)) + '% of the original image ')
